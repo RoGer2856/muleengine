eca5893c144fc853043cd981bc32cac484c98ebf
Gergo Roth <roth@bytifex.com>
2022-11-27 21:48:12 +0100

    feat: when a handler is removed, then sdl2_opengl_muleengine's renderer system will release it

--- src/bin/game_2/main.rs
+++ src/bin/game_2/main.rs
@@ -3,15 +3,15 @@
 use std::sync::Arc;
 
 use game_2::{
-    main_loop::MainLoop, systems::spectator_camera_controller::SpectatorCameraControllerSystem,
+    main_loop::MainLoop,
+    systems::{
+        game_manager::GameManager, spectator_camera_controller::SpectatorCameraControllerSystem,
+    },
 };
 use muleengine::{
     asset_container::AssetContainer,
     asset_reader::AssetReader,
     image_container::ImageContainer,
-    mesh::{Material, MaterialTexture, MaterialTextureType, TextureMapMode},
-    mesh_creator,
-    renderer::renderer_client::RendererClient,
     renderer::renderer_system::Renderer,
     scene_container::SceneContainer,
     service_container::ServiceContainer,
@@ -20,7 +20,7 @@ use muleengine::{
 };
 use parking_lot::RwLock;
 use sdl2_opengl_muleengine::{systems::renderer, GLProfile, Sdl2GLContext};
-use vek::{Transform, Vec2, Vec3};
+use vek::Vec2;
 
 fn main() {
     env_logger::builder()
@@ -83,6 +83,7 @@ async fn async_main() {
         let renderer_system = Renderer::new(renderer_impl);
         let renderer_client = renderer_system.client();
 
+        system_container.add_system(GameManager::new(service_container, renderer_client.clone()));
         system_container.add_system(SpectatorCameraControllerSystem::new(
             renderer_client.clone(),
             sdl2_gl_context.clone(),
@@ -93,11 +94,6 @@ async fn async_main() {
 
         (system_container, renderer_client)
     };
-
-    tokio::spawn(populate_with_objects(
-        service_container,
-        renderer_client.clone(),
-    ));
 
     let event_receiver = sdl2_gl_context.read().event_receiver();
 
@@ -144,159 +140,4 @@ pub fn init_services() -> ServiceContainer {
     service_container.insert(AssetContainer::new(&service_container));
 
     service_container
-}
-
-async fn populate_with_objects(
-    service_container: ServiceContainer,
-    renderer_client: RendererClient,
-) {
-    let asset_container_arc = service_container.get_service::<AssetContainer>().unwrap();
-    let asset_container = asset_container_arc.read().clone();
-
-    add_skybox(asset_container.clone(), renderer_client.clone()).await;
-
-    {
-        let mut transform = Transform::<f32, f32, f32>::default();
-        transform.position.x = -2.0;
-        transform.position.z = -5.0;
-
-        let mesh = Arc::new(mesh_creator::capsule::create(0.5, 2.0, 16));
-
-        let shader_handler = renderer_client
-            .create_shader("Assets/shaders/lit_wo_normal".to_string())
-            .await
-            .unwrap();
-        let material_handler = renderer_client
-            .create_material(mesh.get_material().clone())
-            .await
-            .unwrap();
-        let mesh_handler = renderer_client.create_mesh(mesh).await.unwrap();
-        let renderer_object_handler = renderer_client
-            .create_renderer_object_from_mesh(mesh_handler, shader_handler, material_handler)
-            .await
-            .unwrap();
-        renderer_client
-            .add_renderer_object(renderer_object_handler, transform)
-            .await
-            .unwrap();
-    }
-
-    {
-        let mut transform = Transform::<f32, f32, f32>::default();
-        transform.position.z = -5.0;
-
-        // let scene_path = "Assets/objects/MonkeySmooth.obj";
-        let scene_path = "Assets/demo/wall/wallTextured.fbx";
-        // let scene_path = "Assets/sponza/sponza.fbx";
-        // let scene_path = "Assets/objects/skybox/Skybox.obj";
-        let scene = asset_container
-            .scene_container()
-            .write()
-            .get_scene(
-                scene_path,
-                &asset_container.asset_reader().read(),
-                &mut asset_container.image_container().write(),
-            )
-            .unwrap();
-
-        let shader_handler = renderer_client
-            .create_shader("Assets/shaders/lit_normal".to_string())
-            .await
-            .unwrap();
-        for mesh in scene.meshes_ref().iter() {
-            match &mesh {
-                Ok(mesh) => {
-                    let material_handler = renderer_client
-                        .create_material(mesh.get_material().clone())
-                        .await
-                        .unwrap();
-                    let mesh_handler = renderer_client.create_mesh(mesh.clone()).await.unwrap();
-                    let renderer_object_handler = renderer_client
-                        .create_renderer_object_from_mesh(
-                            mesh_handler,
-                            shader_handler.clone(),
-                            material_handler,
-                        )
-                        .await
-                        .unwrap();
-                    renderer_client
-                        .add_renderer_object(renderer_object_handler, transform)
-                        .await
-                        .unwrap();
-                }
-                Err(e) => {
-                    log::warn!("Invalid mesh in scene, path = {scene_path}, error = {e:?}")
-                }
-            }
-        }
-    }
-}
-
-async fn add_skybox(asset_container: AssetContainer, renderer_client: RendererClient) {
-    let transform = Transform::<f32, f32, f32>::default();
-
-    let scene_path = "Assets/objects/skybox/Skybox.obj";
-    let scene = asset_container
-        .scene_container()
-        .write()
-        .get_scene(
-            scene_path,
-            &asset_container.asset_reader().read(),
-            &mut asset_container.image_container().write(),
-        )
-        .unwrap();
-
-    if scene.meshes_ref().len() == 6 {
-        let texture_paths = [
-            "Assets/objects/skybox/skyboxRight.png",
-            "Assets/objects/skybox/skyboxLeft.png",
-            "Assets/objects/skybox/skyboxTop.png",
-            "Assets/objects/skybox/skyboxBottom.png",
-            "Assets/objects/skybox/skyboxFront.png",
-            "Assets/objects/skybox/skyboxBack.png",
-        ];
-
-        let shader_handler = renderer_client
-            .create_shader("Assets/shaders/unlit".to_string())
-            .await
-            .unwrap();
-
-        for (index, texture_path) in texture_paths.iter().enumerate() {
-            let material = Material {
-                textures: vec![MaterialTexture {
-                    image: asset_container
-                        .image_container()
-                        .write()
-                        .get_image(texture_path, &asset_container.asset_reader().read()),
-                    texture_type: MaterialTextureType::Albedo,
-                    texture_map_mode: TextureMapMode::Clamp,
-                    blend: 0.0,
-                    uv_channel_id: 0,
-                }],
-                opacity: 1.0,
-                albedo_color: Vec3::broadcast(1.0),
-                shininess_color: Vec3::broadcast(0.0),
-                emissive_color: Vec3::broadcast(0.0),
-            };
-
-            let mesh = scene.meshes_ref()[index].as_ref().unwrap().clone();
-
-            let material_handler = renderer_client.create_material(material).await.unwrap();
-            let mesh_handler = renderer_client.create_mesh(mesh).await.unwrap();
-            let renderer_object_handler = renderer_client
-                .create_renderer_object_from_mesh(
-                    mesh_handler,
-                    shader_handler.clone(),
-                    material_handler,
-                )
-                .await
-                .unwrap();
-            renderer_client
-                .add_renderer_object(renderer_object_handler, transform)
-                .await
-                .unwrap();
-        }
-    } else {
-        panic!("Skybox does not contain exactly 6 meshes");
-    }
 }
