bb0c78936c5cae70c459ac9e3ea539f8620b0ae0
Gergo Roth <roth@bytifex.com>
2022-12-30 16:05:10 +0100

    bug: corrected check of shader compilation

--- sdl2_opengl_muleengine/src/opengl_utils/shader.rs
+++ sdl2_opengl_muleengine/src/opengl_utils/shader.rs
@@ -21,6 +21,7 @@ impl ShaderType {
 
 #[derive(Debug)]
 pub enum ShaderCreationError {
+    ShaderSourceContainsNulInsideString,
     CompilationErrorToString(FromUtf8Error),
     CompilationError { error_msg: String },
 }
@@ -33,7 +34,9 @@ impl Shader {
     pub fn new(shader_type: ShaderType, source_code: &str) -> Result<Self, ShaderCreationError> {
         let shader_id = unsafe {
             let shader_id = gl::CreateShader(shader_type.to_gl_shader_type());
+
             let lengths = [source_code.len()];
+
             gl::ShaderSource(
                 shader_id,
                 1,
@@ -49,22 +52,28 @@ impl Shader {
             let mut info_log_length = 0;
             let mut actual_info_log_length: i32 = 0;
 
-            gl::GetShaderiv(shader_id, gl::INFO_LOG_LENGTH, &mut info_log_length);
+            let mut is_compiled = 0;
+            gl::GetShaderiv(shader_id, gl::COMPILE_STATUS, &mut is_compiled);
 
-            if info_log_length > 0 {
-                let mut error_log = vec![0u8; info_log_length as usize + 1];
-                gl::GetShaderInfoLog(
-                    shader_id,
-                    info_log_length,
-                    &mut actual_info_log_length,
-                    error_log.as_mut_ptr() as *mut i8,
-                );
-                error_log.resize(actual_info_log_length as usize, 0);
+            if is_compiled == 0 {
+                gl::GetShaderiv(shader_id, gl::INFO_LOG_LENGTH, &mut info_log_length);
 
-                let error_msg = String::from_utf8(error_log)
-                    .map_err(ShaderCreationError::CompilationErrorToString)?;
-
-                Err(ShaderCreationError::CompilationError { error_msg })?
+                if info_log_length > 0 {
+                    let mut error_log = vec![0u8; info_log_length as usize + 1];
+                    gl::GetShaderInfoLog(
+                        shader_id,
+                        info_log_length,
+                        &mut actual_info_log_length,
+                        error_log.as_mut_ptr() as *mut i8,
+                    );
+                    error_log.resize(actual_info_log_length as usize, 0);
+    
+                    let error_msg = String::from_utf8(error_log)
+                        .map_err(ShaderCreationError::CompilationErrorToString)?;
+                    
+                    Err(ShaderCreationError::CompilationError { error_msg })?
+                }
+   
             }
 
             shader_id
